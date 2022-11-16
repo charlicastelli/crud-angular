d22cd326f2ce4510ee93505925b60bf2e9d4a658
Charli Castelli <charli.castelli@inobram.local>
2022-11-16 15:13:53 -0300

    first-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Verifica se o caminho está vazio e redireciona para componente courses
+  { path: '', pathMatch: 'full', redirectTo: 'courses' },
+  // Caminho courses
+  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { CoursesService } from '../services/courses.service';
+import { Course } from './../model/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [];
+  displayedColumns = ['name', 'category'];
+
+  constructor(private coursesService : CoursesService) { 
+    
+  }
+
+  ngOnInit(): void {
+    this.courses = this.coursesService.list();
+  }
+
+}
