96138ed606fc2f5bc42bb80cb863000d73f15e44
Charli Castelli <charli.castelli@inobram.local>
2022-11-21 17:25:05 -0300

    salvando no banco de dados

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,24 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay, first, tap } from 'rxjs';
+import { delay, first, Observable, tap } from 'rxjs';
 
 import { Course } from './../model/course';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoursesService {
-
   private readonly API = 'api/courses';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   list() {
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
-      delay(1000),
-      tap(courses => console.log(courses))
+      // delay(1000),
+      tap((courses) => console.log(courses))
     );
   }
-  
+
+  save(record: Course) {
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
 }
